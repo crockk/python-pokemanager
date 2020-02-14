e36600dcb956d53278750d41a48896da363cbb42
bakedSpaceTime <tushyais@gmail.com>
2020-02-13 23:36:06 -0800

    Add function prototypes form UML

---
+++ party_manager.py
@@ -0,0 +1,40 @@
+"""
+Author: Tushya Iyer, Nolan Crocks
+ACIT 2515
+Date: 2/13/2020
+"""
+
+from party_member import PartyMember
+
+from typing import List
+
+class PartyManager():
+
+    def __init__(self, player_name: str) -> None:
+        pass
+
+
+    def add_party_member(self, new_member: PartyMember) -> None:
+        pass
+
+
+    def withdraw_party_member(self, id: int) -> None:
+        pass
+
+
+    def release_party_member(self, id: int) -> None:
+        pass
+
+
+    def get_members_by_types(self, type: tuple) -> List:
+        pass
+
+
+    def get_member_by_id(self, id: int) -> PartyMember:
+        pass
+
+
+    def get_stats(self) -> PokeStats:
+        pass
+
+
---
+++ party_member.py
@@ -0,0 +1,74 @@
+"""
+Author: Tushya Iyer, Nolan Crocks
+ACIT 2515
+Date: 2/13/2020
+"""
+
+from datetime import date
+
+class PartyMember():
+
+    def __init__(self, id: int, species: str, source: str, nickname: str = None, item: str = None) -> None:
+	    pass
+
+
+    @property
+    def id(self) -> int:
+        pass
+
+
+    @property
+    def species(self) -> str:
+        pass
+
+
+    @property
+    def name(self) -> str:
+        pass
+
+
+    @name.setter
+    def name(self, new_name: str) -> None:
+        pass
+
+
+    @property
+    def in_part(self) -> bool:
+        pass
+
+
+    @property
+    def weight(self) -> float:
+        pass
+
+
+    @property
+    def height(self) -> float:
+        pass
+
+
+    @property
+    def description(self) -> str:
+        pass
+
+
+    @property
+    def source(self) -> str:
+        pass
+
+
+    @property
+    def date_aquired(self) -> date:
+        pass
+
+
+    @property
+    def held_item(self) -> str:
+        pass
+
+
+    @held_item.setter
+    def held_item(self, item: str) -> None:
+        pass
+
+
