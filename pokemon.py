bd46904f0ee51a8dcfca83676f883e9582d732bb
bakedSpaceTime <tushyais@gmail.com>
2020-02-17 14:03:36 -0800

    add funciton prototypes from UML

---
+++ pokemon.py
@@ -0,0 +1,101 @@
+"""
+Author: Tushya Iyer, Nolan Crocks
+ACIT 2515
+Date: 2/17/2020
+"""
+
+from typing import List
+
+class Pokemon():
+    
+    def __init__(self, id: int, species_id: int, source: str, nickname: str = None, item: str = None, ability: str = None) -> None:
+	    pass
+
+    
+    def _level_up(self) -> None:
+        pass
+
+
+    def _knock_out(self) -> None:
+        pass
+
+
+    @property
+    def id(self) -> int:
+        pass
+
+
+    @property
+    def pokedex(self) -> List:
+        pass
+
+
+    @property
+    def moves(self) -> List:
+        pass
+
+
+    @property
+    def xp_till_next_level(self) -> int:
+        pass
+
+
+    @property
+    def level(self) -> int:
+        pass
+
+
+    @property
+    def ability(self) -> str:
+        pass
+
+
+    @property
+    def type(self) -> str:
+        pass
+
+
+    @property
+    def attack(self) -> int:
+        pass
+
+
+    @property
+    def speed(self) -> int:
+        pass
+
+
+    @property
+    def defense(self) -> int:
+        pass
+
+
+    @property
+    def total_hp(self) -> int:
+        pass
+
+
+    @property
+    def current_hp(self) -> int:
+        pass
+
+
+    @property
+    def is_KO(self) -> bool:
+        pass
+
+
+    def use_move(self, move: str) -> None:
+        pass
+
+
+    def add_xp(self, xp_increse: int) -> None:
+        pass
+
+
+    def heal(self, health_increase: int) -> None:
+        pass
+
+
+    def damage(self, health_decrease: int) -> None:
+        pass
