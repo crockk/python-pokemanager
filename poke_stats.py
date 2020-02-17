e683d0c1cd47b59c8d80b317614c21540bc83ba4
bakedSpaceTime <tushyais@gmail.com>
2020-02-17 14:19:29 -0800

    add funciton prototypes from UML

---
+++ poke_stats.py
@@ -0,0 +1,31 @@
+"""
+Author: Tushya Iyer, Nolan Crocks
+ACIT 2515
+Date: 2/17/2020
+"""
+
+from party_manager import PartyManager
+from typing import List
+
+class PokeStats():
+
+    def __init__(self, Party: PartyManager) -> None:
+        pass
+
+
+    def _get_total_by_type(self, type: str) -> dict:
+        pass
+
+
+    def _get_total_eggs(self) -> int:
+        pass
+
+
+    def _get_total_KO(self) -> int:
+        pass
+
+
+    def _get_total_steps(self, type: str) -> int:
+        pass
+
+
