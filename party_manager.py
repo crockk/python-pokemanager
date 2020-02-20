0b0aa50c451eae94544f535297198732e17988df
bakedSpaceTime <tushyais@gmail.com>
2020-02-19 23:04:27 -0800

    add pokedex num to pokedex entry

--- party_manager.py
+++ party_manager.py
@@ -29,16 +29,16 @@ class PartyManager:
     random.seed(13)
 
     _POKEDEX = {
-        1: ("Bulbasaur", "Grass"),
-        2: ("Charmander", "Fire"),
-        3: ("Squirtle", "Water"),
-        4: ("Pikachu", "Electric"),
-        5: ("Turtwig", "Grass"),
-        6: ("Chimchar", "Fire"),
-        7: ("Piplup", "Water"),
-        8: ("Kyogre", "Water"),
-        9: ("Groudon", "Ground"),
-        10: ("Rayquaza", "Flying/Dragon")
+        1: (1, "Bulbasaur", "Grass"),
+        2: (2, "Charmander", "Fire"),
+        3: (3, "Squirtle", "Water"),
+        4: (4, "Pikachu", "Electric"),
+        5: (5, "Turtwig", "Grass"),
+        6: (6, "Chimchar", "Fire"),
+        7: (7, "Piplup", "Water"),
+        8: (8, "Kyogre", "Water"),
+        9: (9, "Groudon", "Ground"),
+        10: (10, "Rayquaza", "Flying/Dragon")
     }
 
     _ID = 1
