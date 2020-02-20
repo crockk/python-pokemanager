e8ea3605e0eaab9f5c2cbbfa859b185deedd28ec
crockk <nolancrooks@outlook.com>
2020-02-20 10:15:08 -0800

    Added pokedex file

--- party_manager.py
+++ party_manager.py
@@ -10,6 +10,7 @@ from egg import Egg
 from poke_stats import PokeStats
 from typing import List
 import random
+from pokedex import Pokedex
 
 
 class PartyManager:
@@ -28,19 +29,7 @@ class PartyManager:
     """
     random.seed(13)
 
-    _POKEDEX = {
-        1: (1, "Bulbasaur", "Grass"),
-        2: (2, "Charmander", "Fire"),
-        3: (3, "Squirtle", "Water"),
-        4: (4, "Pikachu", "Electric"),
-        5: (5, "Turtwig", "Grass"),
-        6: (6, "Chimchar", "Fire"),
-        7: (7, "Piplup", "Water"),
-        8: (8, "Kyogre", "Water"),
-        9: (9, "Groudon", "Ground"),
-        10: (10, "Rayquaza", "Flying/Dragon")
-    }
-
+    _POKEDEX = Pokedex
     _ID = 1
 
     def __init__(self, player_name: str) -> None:
