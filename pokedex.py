9c8681a24cd2849389a50cf101214bea4c68d49e
bakedSpaceTime <tushyais@gmail.com>
2020-02-24 15:39:18 -0800

    remove pokedex id from etries

--- pokedex.py
+++ pokedex.py
@@ -1,12 +1,12 @@
 Pokedex = {
-    1: (1, "Bulbasaur", "Grass"),
-    2: (2, "Charmander", "Fire"),
-    3: (3, "Squirtle", "Water"),
-    4: (4, "Pikachu", "Electric"),
-    5: (5, "Turtwig", "Grass"),
-    6: (6, "Chimchar", "Fire"),
-    7: (7, "Piplup", "Water"),
-    8: (8, "Kyogre", "Water"),
-    9: (9, "Groudon", "Ground"),
-    10: (10, "Rayquaza", "Flying/Dragon")
+    1: ("Bulbasaur", "Grass"),
+    2: ("Charmander", "Fire"),
+    3: ("Squirtle", "Water"),
+    4: ("Pikachu", "Electric"),
+    5: ("Turtwig", "Grass"),
+    6: ("Chimchar", "Fire"),
+    7: ("Piplup", "Water"),
+    8: ("Kyogre", "Water"),
+    9: ("Groudon", "Ground"),
+    10: ("Rayquaza", "Flying/Dragon")
 }
