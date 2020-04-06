46c594df30a66fbc8cbab56ee78e3caf96237e19
Nolan <nolancrooks@outlook.com>
2020-04-06 15:09:17 -0700

    Updated create/drop tables files so they create and drop all tables

--- db_module/create_tables.py
+++ db_module/create_tables.py
@@ -1,4 +1,6 @@
 from .db import db
 from pokemodule.party_manager import PartyManager
+from pokemodule.egg import Egg
+from pokemodule.pokemon import Pokemon
 
-db.create_tables([PartyManager])
+db.create_tables([PartyManager, Pokemon, Egg])
--- db_module/drop_tables.py
+++ db_module/drop_tables.py
@@ -1,4 +1,6 @@
 from .db import db
 from pokemodule.party_manager import PartyManager
+from pokemodule.egg import Egg
+from pokemodule.pokemon import Pokemon
 
-db.drop_tables([PartyManager])
+db.drop_tables([PartyManager, Pokemon, Egg])
